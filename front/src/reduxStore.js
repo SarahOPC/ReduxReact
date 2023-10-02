cd4472b30b9f338b410661d29a3339ecc0b6ed14
Sarah Raguin <sarahraguin@gmail.com>
2023-10-02 14:28:29 +0200

    Adding redux thunk to handle asynchronous actions and axios to handle HTTP requests from the react app

--- front/src/reduxStore.js
+++ front/src/reduxStore.js
@@ -1,25 +1,28 @@
 import { configureStore, createSlice } from "@reduxjs/toolkit";
 
+// We use a slice to define a piece of the application's state and the actions that can be performed on that state
+// A slice represents a single unit of Redux state.
+// It’s a collection of reducer logic and actions for a single feature in the app, typically defined together in a single file.
+
 const userSlice = createSlice({
     name: "user",
-    initialState: [
-        {id: "userId", userFirstName: "userFirstName", userLastName: "userLastName"},
-        // should have the element from the database ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    ],
+    initialState: [],
     reducers: {
+        // {type: user/changeFirstName, payload: "new firstName"}
+        // splice(userIndex, 1, {userFirstName: action.payload}) removes firstname at position "userIndex" one element and replace it by action.payload
         changeFirstName: (state, action) => {
-            // {type: user/changeFirstName, payload: "new firstName"}
-            // splice(userIndex, 1, {userFirstName: action.payload}) removes firstname at position "userIndex" one element and replace it by action.payload
             const userIndex = state.findIndex(user => user.id === "userId");
-            if(userIndex !== -1) { //returns the index of the element if it's found or -1 if it's not found
+            // returns the index of the element if it's found or -1 if it's not found
+            if(userIndex !== -1) {
                 state.splice(userIndex, 1, {userFirstName: action.payload});
             }
         },
+        // {type: user/changeLastName, payload: "new lastName"}
+        // splice(userIndex, 1, {userLastName: action.payload}) removes lastname at position "userIndex" one element and replace it by action.payload
         changeLastName: (state, action) => {
-            // {type: user/changeLastName, payload: "new lastName"}
-            // splice(userIndex, 1, {userLastName: action.payload}) removes lastname at position "userIndex" one element and replace it by action.payload
             const userIndex = state.findIndex(user => user.id === "userId");
-            if(userIndex !== -1) { //returns the index of the element if it's found or -1 if it's not found
+            // returns the index of the element if it's found or -1 if it's not found
+            if(userIndex !== -1) {
                 state.splice(userIndex, 1, {userLastName: action.payload});
             }
         },
